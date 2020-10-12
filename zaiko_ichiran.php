73c65d92fb0d106c6a781e9901288dfc7ab82cdc
190261fo <69778270+190261fo@users.noreply.github.com>
2020-10-12 10:04:58 +0900

    開発工程(ログイン機能) -> ①名前、パスワードを入れる変数の初期化 -> ②ログインボタンが押されたかを判定
    開発工程(書籍一覧)
    -> ⑤データベースへ接続し、接続情報を変数に保存
    -> ⑥データベースで使用文字コードを「UTF8」に
    -> ⑦書籍テーブルから書籍情報を取得SQLを実行また実行結果を変数に保存
    -> ⑩SQLの実行結果の変数から1レコードのデータを取り出すレコードがない場合はループを終了
    -> ⑪extract変数を使用し、1レコードのデータを渡す

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -13,16 +13,32 @@
 //①セッションを開始する
 
 //②SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ②の処理を書く */){
-	//③SESSIONの「error2」に「ログインしてください」と設定する。
-	//④ログイン画面へ遷移する。
-}
+// if (/* ②の処理を書く */){
+// 	//③SESSIONの「error2」に「ログインしてください」と設定する。
+// 	//④ログイン画面へ遷移する。
+// }
 
 //⑤データベースへ接続し、接続情報を変数に保存する
+$db_name = "zaiko2020_yse";
+$host = "localhost";
+$user_name = "zaiko2020_yse";
+$password = "2020zaiko";
+$mysqli = new mysqli($host, $user_name, $password, $db_name);
 
 //⑥データベースで使用する文字コードを「UTF8」にする
+if ($mysqli->connect_error) {
+	echo $mysqli->connect_error;
+	exit();
+} else {
+	// echo "ok! <br>"; # 接続できていれば表示される
+	$mysqli->set_charset("utf-8");
+}
+	
 
 //⑦書籍テーブルから書籍情報を取得するSQLを実行する。また実行結果を変数に保存する
+$sql = "SELECT * FROM books";
+$result = $mysqli->query($sql)
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -44,9 +60,9 @@ if (/* ②の処理を書く */){
 				 * ⑧SESSIONの「success」にメッセージが設定されているかを判定する。
 				 * 設定されていた場合はif文の中に入る。
 				 */ 
-				if(/* ⑧の処理を書く */){
-					//⑨SESSIONの「success」の中身を表示する。
-				}
+				// if(/* ⑧の処理を書く */){
+				// 	//⑨SESSIONの「success」の中身を表示する。
+				// }
 				?>
 			</div>
 			
@@ -81,19 +97,23 @@ if (/* ②の処理を書く */){
 					<tbody>
 						<?php
 						//⑩SQLの実行結果の変数から1レコードのデータを取り出す。レコードがない場合はループを終了する。
-						while(/* ⑩の処理を書く */){
-							//⑪extract変数を使用し、1レコードのデータを渡す。
+						if ($result = $mysqli->query($sql)) {
+							while($row = $result->fetch_assoc()){
+									// echo "{$row['id']}　{$row['title']}　{$row['author']}　{$row['salesDate']}　{$row['isbn']}　{$row['price']}　{$row['stock']}"."<br>";
+						
+								//⑪extract変数を使用し、1レコードのデータを渡す。
 
-							echo "<tr id='book'>";
-							echo "<td id='check'><input type='checkbox' name='books[]'value="./* ⑫IDを設定する */."></td>";
-							echo "<td id='id'>/* ⑬IDを表示する */</td>";
-							echo "<td id='title'>/* ⑭titleを表示する */</td>";
-							echo "<td id='author'>/* ⑮authorを表示する */</td>";
-							echo "<td id='date'>/* ⑯salesDateを表示する */</td>";
-							echo "<td id='price'>/* ⑰priceを表示する */</td>";
-							echo "<td id='stock'>/* ⑱stockを表示する */</td>";
+								echo "<tr>";
+								// echo "<td><input type='checkbox' name='books[]'value="./* ⑫IDを設定する */."></td>";
+								echo "<td>/* ⑬IDを表示する */</td>";
+								echo "<td>/* ⑭titleを表示する */</td>";
+								echo "<td>/* ⑮authorを表示する */</td>";
+								echo "<td>/* ⑯salesDateを表示する */</td>";
+								echo "<td>/* ⑰priceを表示する */</td>";
+								echo "<td>/* ⑱stockを表示する */</td>";
 
-							echo "</tr>";
+								echo "</tr>";
+							}
 						}
 						?>
 					</tbody>
