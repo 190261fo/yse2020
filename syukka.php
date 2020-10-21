88904f0bc1fb74d27d684bd888bdc423cbb147cb
tomomichi549 <190257oi@yse-c.net>
2020-10-21 11:58:43 +0900

    出荷、出荷確認修正

--- syukka.php
+++ syukka.php
@@ -13,10 +13,10 @@
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-// if (/* ①の処理を行う */) {
-// 	//②セッションを開始する
-// }
-
+if (session_status() == PHP_SESSION_NONE /* ①.の処理を行う */) {
+	// 	//②セッションを開始する
+	session_start();
+}
 // //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
 // if (/* ③の処理を書く */){
 // 	//④SESSIONの「error2」に「ログインしてください」と設定する。
@@ -30,12 +30,12 @@ $user_name = 'zaiko2020_yse';
 $password = '2020zaiko';
 $mysqli = new mysqli($host, $user_name, $password, $db_name);
 
-if($mysqli->connect_error){
+if ($mysqli->connect_error) {
 	echo $mysqli->connect_error;
 	exit();
-}else{	
+} else {
 
-//⑦データベースで使用する文字コードを「UTF8」にする
+	//⑦データベースで使用する文字コードを「UTF8」にする
 	$mysqli->set_charset('utf8');
 }
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
@@ -44,7 +44,8 @@ if($mysqli->connect_error){
 // 	//⑩在庫一覧画面へ遷移する。
 // }
 
-function getId($id,$con){
+function getId($id, $con)
+{
 	/* 
 	 * ⑪書籍を取得するSQLを作成する実行する。
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
@@ -53,89 +54,92 @@ function getId($id,$con){
 	$sql = "SELECT * FROM books WHERE id=$id";
 	$result = $con->query($sql);
 	//⑫実行した結果から1レコード取得し、returnで値を返す。
-	if($result){
+	if ($result) {
 		return $result->fetch_assoc();
 	}
 }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>出荷</title>
 	<link rel="stylesheet" href="css/ichiran.css" type="text/css" />
 </head>
+
 <body>
-<!-- ヘッダ -->
-<div id="header">
-	<h1>出荷</h1>
-</div>
+	<!-- ヘッダ -->
+	<div id="header">
+		<h1>出荷</h1>
+	</div>
 
-<!-- メニュー -->
-<div id="menu">
-	<nav>
-		<ul>
-			<li><a href="zaiko_ichiran.php?page=1">書籍一覧</a></li>
-		</ul>
-	</nav>
-</div>
+	<!-- メニュー -->
+	<div id="menu">
+		<nav>
+			<ul>
+				<li><a href="zaiko_ichiran.php?page=1">書籍一覧</a></li>
+			</ul>
+		</nav>
+	</div>
 
-<form action="syukka_kakunin.php" method="post">
-	<div id="pagebody">
-		<!-- エラーメッセージ -->
-		<div id="error">
-		<?php
-		/*
+	<form action="syukka_kakunin.php" method="post">
+		<div id="pagebody">
+			<!-- エラーメッセージ -->
+			<div id="error">
+				<?php
+				/*
 		 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
 		 * 設定されていた場合はif文の中に入る。
-		 */ 
-		// if(/* ⑬の処理を書く */){
-		// 	//⑭SESSIONの「error」の中身を表示する。
-		// }
-		?>
-		</div>
-		<div id="center">
-			<table>
-				<thead>
-					<tr>
-						<th id="id">ID</th>
-						<th id="book_name">書籍名</th>
-						<th id="author">著者名</th>
-						<th id="salesDate">発売日</th>
-						<th id="itemPrice">金額(円)</th>
-						<th id="stock">在庫数</th>
-						<th id="in">出荷数</th>
-					</tr>
-				</thead>
-				<?php 
-				/*
+		 */
+				// if(/* ⑬の処理を書く */){
+				// 	//⑭SESSIONの「error」の中身を表示する。
+				// }
+				?>
+			</div>
+			<div id="center">
+				<table>
+					<thead>
+						<tr>
+							<th id="id">ID</th>
+							<th id="book_name">書籍名</th>
+							<th id="author">著者名</th>
+							<th id="salesDate">発売日</th>
+							<th id="itemPrice">金額(円)</th>
+							<th id="stock">在庫数</th>
+							<th id="out">出荷数</th>
+						</tr>
+					</thead>
+					<?php
+					/*
 				 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
 				 */
-				foreach($_POST["books"] as $book){
-					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
-					$extract = getId($book, $mysqli);
-				?>
-				<input type="hidden" value="<?php $extract["id"]; ?>" name="books[]">
-				<tr>
+					foreach ($_POST["books"] as $book) {
+						// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
+						$extract = getId($book, $mysqli);
+					?>
+						<input type="hidden" value="<?php echo $extract["id"]; ?>" name="books[]">
+						<tr>
 							<td><?php echo $extract["id"]; ?></td>
 							<td><?php echo $extract["title"]; ?></td>
 							<td><?php echo $extract["author"]; ?></td>
 							<td><?php echo $extract["salesDate"]; ?></td>
 							<td><?php echo $extract["price"]; ?></td>
 							<td><?php echo $extract["stock"]; ?></td>
-					<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
-				</tr>
-				<?php
-				}
-				?>
-			</table>
-			<button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
+							<td><input type='text' name='out[]' size='5' maxlength='11' required></td>
+						</tr>
+					<?php
+					}
+					?>
+				</table>
+				<button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
+			</div>
 		</div>
+	</form>
+	<!-- フッター -->
+	<div id="footer">
+		<footer>株式会社アクロイト</footer>
 	</div>
-</form>
-<!-- フッター -->
-<div id="footer">
-	<footer>株式会社アクロイト</footer>
-</div>
 </body>
+
 </html>
