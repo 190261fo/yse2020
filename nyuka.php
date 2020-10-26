ce7cf1b9ddd3e6b54c499572a51260ec3166a36d
190256 <190256hr@yse-c.net>
2020-10-26 10:31:14 +0900

    開発工程(入荷) ③SESSIONの「login」フラグがfalseか判定「login」フラグがfalseの場合はif文の中に入る ④SESSIONの「error2」に「ログインしてください」と設定 ⑤ログイン画面へ遷移

--- nyuka.php
+++ nyuka.php
@@ -20,10 +20,12 @@ if (session_status() == PHP_SESSION_NONE /* ①.の処理を行う */) {
 }
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-// if (/* ③の処理を書く */){
-// 	//④SESSIONの「error2」に「ログインしてください」と設定する。
-// 	//⑤ログイン画面へ遷移する。
-// }
+if ($_SESSION['login']==false){
+	//④SESSIONの「error2」に「ログインしてください」と設定する。
+	$_SESSION["error2"] = "ログインしてください";
+	//⑤ログイン画面へ遷移する。
+	header("Location: login.php");
+}
 
 //⑥データベースへ接続し、接続情報を変数に保存する
 $db_name = 'zaiko2020_yse';
