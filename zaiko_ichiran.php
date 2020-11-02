6d15cfff8e29ffee2a933b4ef5b20598d20ad8f4
190261fo <69778270+190261fo@users.noreply.github.com>
2020-11-02 10:18:53 +0900

    zaiko_ichiran.php に新商品追加ボタン、商品削除ボタン を追加

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -80,6 +80,11 @@ if($mysqli->connect_error){
 				<button type="submit" id="btn1" formmethod="POST" name="decision" value="3" formaction="nyuka.php">入荷</button>
 
 				<button type="submit" id="btn1" formmethod="POST" name="decision" value="4" formaction="syukka.php">出荷</button>
+				<!-- 2つボタン追加 -->
+				<button type="submit" id="btn1" formmethod="POST" name="decision" value="5" formaction="new_product.php">新商品追加</button>
+
+				<button type="submit" id="btn1" formmethod="POST" name="decision" value="6" formaction="delete_product.php">商品削除</button>
+
 			</div>
 			<!-- 中央表示 -->
 			<div id="center">
