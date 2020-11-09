3fb47e14778326bf0324e840c066cc8df8ab4b1f
190256 <190256hr@yse-c.net>
2020-11-09 09:16:41 +0900

    商品検索ボタン追加

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -86,6 +86,7 @@ if($mysqli->connect_error){
 
 				<button type="submit" id="btn1" formmethod="POST" name="decision" value="6" formaction="delete_product.php">商品削除</button>
 
+				<button type="submit" id="btn1" formmethod="POST" name="decision" value="6" formaction="search.php">商品検索</button>
 			</div>
 			<!-- 中央表示 -->
 			<div id="center">
